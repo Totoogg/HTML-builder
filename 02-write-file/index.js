9622ea68a5457994ca06ec6ce1fa806ca3c400af
totoogg <goncharov.vova2014@gamil.com>
2024-01-17 01:32:20 +0300

    feat: add solute task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const { stdin, stdout } = process;
+const path = require('path');
+const fs = require('fs');
+
+stdout.write('Write something\n');
+stdin.on('data', (dataWrite) => {
+  if (dataWrite.toString().trim() === 'exit') process.exit();
+  fs.stat(path.join(__dirname, 'text.txt'), (err, stats) => {
+    if (err) {
+      fs.writeFile(path.join(__dirname, 'text.txt'), dataWrite, (error) => {
+        if (error) return console.error(error.message);
+      });
+    } else {
+      fs.readFile(path.join(__dirname, 'text.txt'), (error, data) => {
+        if (error) return console.error(error.message);
+        const notes = data + dataWrite;
+
+        fs.writeFile(path.join(__dirname, 'text.txt'), notes, (error) => {
+          if (error) return console.error(error.message);
+        });
+      });
+    }
+  });
+});
+
+process.on('SIGINT', function () {
+  process.exit();
+});
+
+process.on('exit', () => stdout.write('Goodbye!'));
