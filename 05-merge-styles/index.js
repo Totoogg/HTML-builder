656b8ebeda27ad0f7eb58b31f0f82b9f6928b635
totoogg <goncharov.vova2014@gamil.com>
2024-01-17 05:25:50 +0300

    feat: add solute task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const fs = require('fs');
+
+const output = fs.createWriteStream(
+  path.join(__dirname, 'project-dist', 'bundle.css'),
+);
+
+fs.stat(path.join(__dirname, 'files-copy'), (err) => {
+  if (err) {
+    fs.writeFile(
+      path.join(__dirname, 'project-dist', 'bundle.css'),
+      '',
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+      },
+    );
+  }
+});
+
+fs.readdir(path.join(__dirname, 'styles'), (err, items) => {
+  items.forEach((x) => {
+    fs.stat(path.join(__dirname, 'styles', x), () => {
+      if (x.split('.').at(-1) === 'css') {
+        const input = fs.createReadStream(
+          path.join(__dirname, 'styles', x),
+          'utf-8',
+        );
+        input.pipe(output);
+      }
+    });
+  });
+});
